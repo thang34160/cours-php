6242bd05f7abec10f8baba160af7a098772a8f7f
thang34160 <thang4@hotmail.com>
2025-04-22 16:51:08 +0200

    ajout exo3

---
+++ exo3/exo3.php
@@ -0,0 +1,35 @@
+<?php
+
+// exo 3.1
+$a =8;
+$b = 4;
+$operation = '-';
+
+if ($operation === '*') {
+    echo ($a * $b) . PHP_EOL;
+}elseif ($operation === '+') {
+    echo ($a + $b). PHP_EOL;
+}elseif ($operation === '-') {
+     echo ($a - $b). PHP_EOL;
+}elseif ($operation === '+') {
+    echo ($a / $b). PHP_EOL;
+};
+
+// exo 3.2
+$result = 5 + 3 * 2;
+echo ("Le résultat SANS changer les priorités est: $result") . PHP_EOL;
+
+$result2 = (5 + 3) * 2;
+echo ("Le résultat en changeant les priorité est:  $result2") . PHP_EOL;
+
+// exo 3.3
+$x = 10;
+echo ($x > 5) . PHP_EOL;
+echo ($x < 5) . PHP_EOL;
+echo ($x == 10) . PHP_EOL;
+echo ($x != 8) . PHP_EOL;
+echo ($x >= 10) . PHP_EOL;
+echo ($x <= 9) . PHP_EOL;
+
+// exo 3.4
+
