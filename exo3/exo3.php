1cfa261a0c7f82063e4320c09880928ba07754e0
thang34160 <thang4@hotmail.com>
2025-04-23 08:14:31 +0200

    echec de l'exo 3.8

--- exo3/exo3.php
+++ exo3/exo3.php
@@ -40,3 +40,34 @@ if ($isAdmin == true && $isLoggedIn == false) {
 }elseif ($isAdmin == true || $isLoggedIn == false) {
     echo ("c'est ok") . PHP_EOL;
 }
+
+if ($isAdmin !== false) {
+    echo ("vous êtes admin") . PHP_EOL;
+};
+
+// exo 3.5
+$score = 15;
+$result = ($score >= 10 ? "réussi" : "échoué");
+echo $result . PHP_EOL;
+
+// exo 3.6
+$n = 4;
+$n += 3;
+echo $n. PHP_EOL;
+$n *= 2;
+echo $n. PHP_EOL;
+$n -= 1;
+echo $n. PHP_EOL;
+$n %= 5;
+echo $n. PHP_EOL;
+
+// exo 3.7
+$prenom = "Alice";
+$nom = "Durand";
+echo("Votre nom complet est $prenom $nom") . PHP_EOL;
+
+// exo 3.8
+$handle = fopen("fopentest.txt", "r");
+
+
+
